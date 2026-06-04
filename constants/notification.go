bda50d100f5ba8cb4bef59d9065c13a5f0bbf6f1
ForbiddenR <126741049+ForbiddenR@users.noreply.github.com>
2026-06-04 07:33:55 +0000

    remove all the things

--- constants/notification.go
+++
@@ -1,8 +0,0 @@
-package constants
-
-const (
-	NotificationTriggerTaskFinish       = "task_finish"
-	NotificationTriggerTaskError        = "task_error"
-	NotificationTriggerTaskEmptyResults = "task_empty_results"
-	NotificationTriggerTaskNever        = "task_never"
-)
