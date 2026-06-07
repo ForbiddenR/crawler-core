1605e280e8df7a82d15a4b7852f6bd13e2ce28b3
ForbiddenR <hostyouposition@outlook.com>
2026-06-07 10:48:47 +0800

    remove old files

--- inject/container.go
+++
@@ -1,9 +0,0 @@
-package inject
-
-import "go.uber.org/dig"
-
-var c = dig.New()
-
-func GetContainer() *dig.Container {
-	return c
-}
--- interfaces/task_hook_service.go
+++
@@ -1,6 +0,0 @@
-package interfaces
-
-type TaskHookService interface {
-	PreActions(Task, Spider, TaskFsService, TaskHandlerService) (err error)
-	PostActions(Task, Spider, TaskFsService, TaskHandlerService) (err error)
-}
